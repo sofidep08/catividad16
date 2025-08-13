2d7dd269b13e56f5ab4c2aec43b6610bcf503505
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-08-13 11:10:12 -0600

    usuarios

--- actividad16.py
+++ actividad16.py
@@ -12,7 +12,7 @@ class Trabajolibros:
     def __init__(self):
         self.lista_libros = []
 
-    def agregar(self):
+    def agregar1(self):
         try:
             titulo=input("Ingrese el título del libro: ")
             autor=input("Ingrese el nombre del autor: ")
@@ -21,18 +21,41 @@ class Trabajolibros:
             self.lista_libros.append(Libros(titulo, autor, publicacion, codigo))
         except ValueError:
             print("El valor ingresado no es valido")
-    def mostrar(self):
+    def mostrar1(self):
         if not self.lista_libros:
             print("No hay libros registrados")
         else:
-            print("\nLista de estudiantes:")
+            print("\nLista de Libros registrados:")
             for i, datos in enumerate(self.lista_libros, start=1):
                 print(f"{i}. {datos.mostrar_libro()}")
             print()
-class usuarios:
+class Usuarios:
     def __init__(self, nombre, carnet,carrera):
         self.nombre = nombre
         self.carnet = carnet
         self.carrera = carrera
     def mostrar_usuario(self):
         return f"Nombre: {self.nombre} - Carnet: {self.carnet} - Carrera: {self.carrera}"
+
+class TrabajoUsuario:
+    def __init__(self):
+        self.lista_usuarios = []
+
+    def agregar2(self):
+        try:
+            nombre=input("Ingrese el nombre: ")
+            carnet=int(input("Ingrese el carnet: "))
+            carrera=input("Ingrese el carrera: ")
+            self.lista_usuarios.append(Usuarios(nombre,carnet,carrera))
+        except ValueError:
+            print("El valor ingresado no es valido")
+    def mostrar2(self):
+        if not self.lista_usuarios:
+            print("No hay libros registrados")
+        else:
+            print("\nLista de usuarios registrados:")
+            for i, datos in enumerate(self.lista_usuarios, start=1):
+                print(f"{i}. {datos.mostrar_libro()}")
+            print()
+
+class Prestamos:
